a9deec8557181fcb8297a22c5ae4a77d9cbb99dd
Vitche Research Team <developer@vitche.com>
2014-03-05 19:40:45 +0200

    1. The first working version of VK API call.

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 var vk = require('nodejs-vk-api');
-console.log(vk);
+vk = new vk({
+    appID: '{ApplicationIdentifier}',
+    appSecret: '{ApplicationSecret}',
+    mode: 'oauth',
+    username: '{UserName}',
+    password: '{Password}'
+});
+vk.on('acquireTokenReady', function() {
+    console.log('VKontakte.Api.acquireTokenReady: ' + vk.getToken());
+    vk.request('newsfeed.get', {}, 'newsfeed.get');
+});
+vk.on('acquireTokenNotReady', function() {
+    console.log('VKontakte.Api.acquireTokenNotReady');
+});
+vk.on('newsfeed.get', function(result) {
+    console.log(result);
+});
+vk.acquireToken();
