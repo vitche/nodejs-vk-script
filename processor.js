5695ef71ac34944d22b22aff02b48ad5d1025853
Vitche Research Team <developer@vitche.com>
2014-11-05 16:15:01 +0200

    1. Support for creating clients by specifying access token in settings.

--- processor.js
+++ processor.js
@@ -4,21 +4,33 @@ var compressor = require('./compressor.js');
 exports.create = function (settings) {
     return {
         client: function () {
-            var client = new vk({
-                appID: settings.applicationIdentifier,
-                appSecret: settings.applicationSecret,
-                mode: 'oauth'
-            });
-            client.on('acquireTokenReady', function () {
-                console.log('VKontakte.Api.acquireTokenReady: ' + client.getToken());
-                client.ready = true;
-            });
-            client.on('acquireTokenNotReady', function () {
-                console.log('VKontakte.Api.acquireTokenNotReady');
-                client.ready = false;
-            });
-            client.acquireToken(settings.userName, settings.password);
-            return client;
+            if (undefined != settings.password) {
+                var client = new vk({
+                    appID: settings.applicationIdentifier,
+                    appSecret: settings.applicationSecret,
+                    mode: 'oauth'
+                });
+                client.on('acquireTokenReady', function () {
+                    console.log('VKontakte.Api.acquireTokenReady: ' + client.getToken());
+                    client.ready = true;
+                });
+                client.on('acquireTokenNotReady', function () {
+                    console.log('VKontakte.Api.acquireTokenNotReady');
+                    client.ready = false;
+                });
+                client.acquireToken(settings.userName, settings.password);
+                return client;
+            } else if (undefined != settings.accessToken) {
+                var client = new vk({
+                    mode: 'oauth'
+                });
+                client.setToken({
+                    token: settings.accessToken
+                });
+                return client;
+            } else {
+                return null;
+            }
         }(),
         process: function (block, callback, tokenReplacements) {
             var self = this;
