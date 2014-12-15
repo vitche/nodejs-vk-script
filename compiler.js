420c40bf40289e12f4fb1bdd7708939541687863
Vitche Research Team <developer@vitche.com>
2014-12-15 19:58:26 +0200

    1. Compiler fixes which stopped scripts from running in the current project.

--- compiler.js
+++ compiler.js
@@ -1,10 +1,13 @@
 exports.compile = function(block) {
     var result = '' + block;
-    result = result.replace(/^(function[ ]*\([ ]*\)[ ]*{[\n]*)/g, '');
+    result = result.replace(/^(function[ ]*\([ ]*\)[ ]*\{[\n]*)/g, '');
     result = result.replace(/([\n]*[ ]*}[ ]*$[\n]*)/g, '');
     result = result.replace(/(^[ ]*|[\n]+[ ]*)/g, '');
-    result = result.replace(/ /g, '%20');
-    result = result.replace(/\+/g, '%2B');
+    // Such code is not compiled at the server side any more
+    // after some changes
+    // probably in the "request" library
+    // result = result.replace(/ /g, '%20');
+    // result = result.replace(/\+/g, '%2B');
     // Compile the VKScript entity projection operator from something
     // closer to JavaScript
     result = result.replace(/.projection./g, '@.');
