44f6b6ad28a409ecccb7da6f646d1a47d2d5a93a
Vitche Research Team <developer@vitche.com>
2015-01-15 17:43:06 +0200

    1. A fix for API version what caused to use HTML form instead of CAPTCHA.

--- patches/vksdk.js
+++ patches/vksdk.js
@@ -1,9 +1,9 @@
 var vk = require('vksdk');
 var patch = require('patch');
 var https = require('https');
-patch.replace(vk.prototype, 'requestServerToken', function(userName, password, _response) {
+patch.replace(vk.prototype, 'requestServerToken', function (userName, password, _response) {
     var responseType = 'event';
-    if ( typeof(_response) === 'function') {
+    if (typeof(_response) === 'function') {
         responseType = 'callback';
     }
     var options = {
@@ -12,14 +12,14 @@ patch.replace(vk.prototype, 'requestServerToken', function(userName, password, _
         path: '/access_token?client_id=' + this.options.appId + '&client_secret=' + this.options.appSecret +
         '&v=' + this.options.version + '&grant_type=password&scope=notify,friends,photos,audio,video,docs,messages,notifications,offline,wall&username=' + userName + '&password=' + password
     };
-    var self  = this;
-    https.get(options, function(res) {
+    var self = this;
+    https.get(options, function (res) {
         var apiResponse = new String();
         res.setEncoding('utf8');
-        res.on('data', function(chunk) {
+        res.on('data', function (chunk) {
             apiResponse += chunk;
         });
-        res.on('end', function() {
+        res.on('end', function () {
             var result = undefined;
             try {
                 result = JSON.parse(apiResponse);
@@ -34,6 +34,65 @@ patch.replace(vk.prototype, 'requestServerToken', function(userName, password, _
                     return self.emit(_response, result);
                 }
                 return self.emit('serverTokenReady', result);
+            }
+        });
+    }).on('error', function (e) {
+        self.emit('http-error', e);
+    });
+});
+patch.replace(vk.prototype, 'request', function (_method, _requestParams, _response) {
+    var responseType = 'event';
+    if (typeof(_response) === 'function') {
+        responseType = 'callback';
+    }
+    var self = this;
+    var params = {
+        'lang': this.options.lang,
+        // This breaks some behavior specific to mobile API
+        // TODO: A way to specify from outside or do not specify at all
+        //'v'     : this.options.version,
+        'https': (this.options.https) ? 1 : 0
+    };
+    if (this.isEmpty(_requestParams) === false) {
+        for (var i in _requestParams) {
+            params[i] = _requestParams[i];
+        }
+    }
+    var requestString = this.buildQuery(params);
+    if (this.options.secure) {
+        if (this.token) {
+            requestString = requestString + '&access_token=' + this.token;
+        }
+        if (this.options.appSecret) {
+            requestString = requestString + '&client_secret=' + this.options.appSecret;
+        }
+    }
+    var options = {
+        host: 'api.vk.com',
+        port: 443,
+        path: '/method/' + _method + '?' + requestString
+    };
+    https.get(options, function (res) {
+        var apiResponse = new String();
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            apiResponse += chunk;
+        });
+        res.on('end', function () {
+            var result = undefined;
+            try {
+                result = JSON.parse(apiResponse);
+            } catch (exception) {
+                // TODO: A better way to handle this exception
+                result = exception;
+            }
+            if (responseType === 'callback' && typeof _response === 'function') {
+                _response(result);
+            } else {
+                if (responseType === 'event' && !!_response) {
+                    return self.emit(_response, result);
+                }
+                return self.emit('done:' + _method, result);
             }
         });
     }).on('error', function (e) {
